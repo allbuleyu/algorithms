3707601e6ce0cdb2ba0c09651e6ff13431ab7b82
Yuli Huang <allbuleyu@163.com>
2018-09-13 18:02:58 +0800

    add depth

---
+++ leetcode/0111-easy-minimum-depth-of-binary-tree.go
@@ -0,0 +1,24 @@
+package leetcode
+
+
+
+/**
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Right != nil || root.Left != nil {
+		return max(minDepth(root.Left), minDepth(root.Right)) + 1
+	}
+
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
--- t.go
+++ t.go
@@ -16,6 +16,24 @@ var arr []int = []int{2,8,7,1, 3, 5,6, 4}
 
 func main() {
 
+	s1 := "123"
+	fmt.Println(&s1, s1)
+
+	s1 = s1[0:1] + " " + s1[1:]
+	fmt.Println(&s1, s1)
+
+
+	i1 := 123
+	i2 := 345
+
+	fmt.Println(&i1, i1)
+
+	i1 = i2
+	fmt.Println(&i1, i1)
+
+
+	return
+
 	//ch2.MergeSort(arr, 0, len(arr))
 
 	//ch2.MergeSortFor(arr)
