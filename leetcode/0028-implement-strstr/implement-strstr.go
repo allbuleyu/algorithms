ef8245866344869d3aea66ea5b176b942f444931
allbuleyu <allbuleyu@163.com>
2019-02-28 11:38:59 +0800

    prob028 优化对比方法

--- leetcode/0028-implement-strstr/implement-strstr.go
+++ leetcode/0028-implement-strstr/implement-strstr.go
@@ -35,3 +35,32 @@ func strStr(haystack string, needle string) int {
 
 	return start
 }
+
+func strstr1(haystack string, needle string) int {
+	m := len(haystack)
+	n := len(needle)
+	if m < n {
+		return -1
+	}
+
+	if needle == "" {
+		return 0
+	}
+
+	start := -1
+
+	// 时间复杂负 (m - n ) * n
+	HS:
+	for i := 0; i < m; i++ {
+		if i + n > m {
+			break HS
+		}
+
+		if haystack[i:i+n] == needle {
+			return i
+		}
+
+	}
+
+	return start
+}
