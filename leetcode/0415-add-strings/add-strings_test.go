9f6eb703947ea0f1f7989f63ba93d687b35b0fb9
allbuleyu <allbuleyu@163.com>
2019-03-07 17:42:38 +0800

    prob415 完成

--- leetcode/0415-add-strings/add-strings_test.go
+++ leetcode/0415-add-strings/add-strings_test.go
@@ -1,1 +1,30 @@
 package prob0415
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_prob0415(t *testing.T)  {
+	ast := assert.New(t)
+
+	tcs := []struct {
+		input string
+		input1 string
+		ans string
+	}{
+		{"1", "9", "10"},
+		{"123", "123", "246"},
+		{"9", "9", "18"},
+		{"0", "0", "0"},
+		{"9", "99", "108"},
+
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, addStrings(tc.input, tc.input1), "输入:%v", tc)
+	}
+}
