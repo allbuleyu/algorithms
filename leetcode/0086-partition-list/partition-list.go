a0359179231edf1f12366e2d3f797bc355289389
allbuleyu <allbuleyu@163.com>
2019-03-13 17:46:53 +0800

    add and solve prob0086

---
+++ leetcode/0086-partition-list/partition-list.go
@@ -0,0 +1,51 @@
+package prob0086
+
+import "github.com/allbuleyu/algorithms/kit"
+
+type ListNode = kit.ListNode
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// 还有一种不好理解的方式,交换节点法,效率一样
+func partition(head *ListNode, x int) *ListNode {
+	var less, lessHead,great, greatHead *ListNode
+	for head != nil {
+		if head.Val < x {
+			if lessHead == nil {
+				lessHead = head
+				less = head
+			}else {
+				less.Next = head
+				less = less.Next
+
+			}
+		}else {
+			if greatHead == nil {
+				greatHead = head
+				great = head
+			}else {
+				great.Next = head
+				great = great.Next
+
+			}
+		}
+
+		head = head.Next
+	}
+
+	if great != nil {
+		great.Next=nil
+	}
+
+	if lessHead == nil {
+		return greatHead
+	}
+
+	less.Next = greatHead
+	return lessHead
+}
---
+++ leetcode/0086-partition-list/partition-list_test.go
@@ -0,0 +1,32 @@
+package prob0086
+
+
+import (
+	"github.com/allbuleyu/algorithms/kit"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"fmt"
+)
+
+func Test_prb00206(t *testing.T)  {
+	ast := assert.New(t)
+
+	// test case
+	tcs := []struct{
+		input []int
+		x int
+		ans []int
+	}{
+		{[]int{1, 4,3,2,5,2}, 3, []int{1,2,2,4,3,5}},
+		{[]int{1,2,3}, 0, []int{1,2,3}},
+		{[]int{1,2,3}, 4, []int{1,2,3}},
+
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		actual := kit.TransferNodes(partition(kit.CreateNodes(tc.input),tc.x))
+		ast.Equal(tc.ans, actual, "输入:%v", tc)
+	}
+}
