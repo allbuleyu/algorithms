6c5aed9ec6e3c386695d7a7745059aa4b95ec143
allbuleyu <allbuleyu@163.com>
2019-03-13 16:09:32 +0800

    prob002 is solved

--- leetcode/0002-add-two-numbers/add-two-numbers.go
+++ leetcode/0002-add-two-numbers/add-two-numbers.go
@@ -1,6 +1,7 @@
 package prob0002
 
 import "github.com/allbuleyu/algorithms/kit"
+type ListNode = kit.ListNode
 
 /**
  * Definition for singly-linked list.
@@ -9,9 +10,94 @@ import "github.com/allbuleyu/algorithms/kit"
  *     Next *ListNode
  * }
  */
-
-type ListNode = kit.ListNode
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	return solutionOptimize(l1,l2)
+}
+
+
+// time 100%, space 100%
+func solutionOptimize(l1, l2 *ListNode) *ListNode {
+	carry := 0
+	head := l1
+
+	for l2 != nil {
+		l1.Val += l2.Val + carry
+		carry = l1.Val / 10
+		l1.Val = l1.Val%10
+
+		if l1.Next == nil && carry == 1 {
+			l1.Next = &ListNode{Val:0}
+		}
+
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+
+	for carry == 1 {
+		l1.Val += carry
+		carry = l1.Val/10
+		l1.Val %= 10
+
+		if carry == 1 {
+			l1.Next = &ListNode{Val:0}
+		}
+
+		l1 = l1.Next
+	}
+
+	return head
+}
+
+// 其实解中sum的空间也可以省略
+func solution1(l1, l2 *ListNode) *ListNode {
+	carry := 0
+	var head = l1
+	for l1 != nil || l2 != nil {
+		sum := 0
+		if l1 != nil {
+			sum += l1.Val
+		}
+
+		if l2 != nil {
+			sum += l2.Val
+		}
+
+		sum += carry
+
+		if sum >= 10 {
+			carry = 1
+		}else {
+			carry = 0
+		}
+
+		// 把计算结果存入l1中,节省空间
+		l1.Val = sum % 10
+
+		if l1.Next == nil {
+
+
+			// 在l2比l1长的情况下,让l1持续加长,
+			//当l2最后一个节点的时候,直接退出,才是正确结果
+			if l2 == nil && carry == 0 {
+				break
+			}
+
+			if l2 != nil && l2.Next == nil && carry == 0 {
+				break
+			}
+			l1.Next = &ListNode{Val:0,Next:nil}
+		}
+
+
+
+		l1 = l1.Next
+
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+
+	//head = reverseList(head)
+
+	return head
 }
--- leetcode/0002-add-two-numbers/add-two-numbers_test.go
+++ leetcode/0002-add-two-numbers/add-two-numbers_test.go
@@ -1,1 +1,33 @@
 package prob0002
+
+import (
+	"github.com/allbuleyu/algorithms/kit"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"fmt"
+)
+
+func Test_prb0002(t *testing.T)  {
+	ast := assert.New(t)
+
+	// test case
+	tcs := []struct{
+		input []int
+		input2 []int
+		ans []int
+	}{
+		{[]int{2,4,3}, []int{5,6,4}, []int{7,0,8}},
+		{[]int{9,9}, []int{1}, []int{0,0,1}},
+		{[]int{1}, []int{9,9}, []int{0,0,1}},
+		{[]int{1,2,3}, []int{2,8,7}, []int{3,0,1,1}},
+
+
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		actual := kit.TransferNodes(addTwoNumbers(kit.CreateNodes(tc.input), kit.CreateNodes(tc.input2)))
+		ast.Equal(tc.ans, actual, "输入:%v", tc)
+	}
+}
