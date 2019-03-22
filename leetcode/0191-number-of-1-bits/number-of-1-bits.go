163a74783045b62c25f0fd957105920ea3896f7c
allbuleyu <allbuleyu@163.com>
2019-03-22 15:05:59 +0800

    prob 191 使用hamming weight解决,并了解原理

--- leetcode/0191-number-of-1-bits/number-of-1-bits.go
+++ leetcode/0191-number-of-1-bits/number-of-1-bits.go
@@ -1,7 +1,15 @@
 package prob0191
 
+const (
+	m1 = 0x55555555
+	m2 = 0x33333333
+	m4 = 0x0f0f0f0f
+	m8 = 0x00ff00ff
+	m16 = 0x0000ffff
+)
+
 func hammingWeight(num uint32) int {
-	return solution3(num)
+	return solution4(num)
 }
 
 func solution1(num uint32) int {
@@ -32,6 +40,7 @@ func solution2(num uint32) int {
 	return bits
 }
 
+// bench 100000000	        19.5 ns/op
 func solution3(num uint32) int {
 	bits := 0
 
@@ -41,4 +50,16 @@ func solution3(num uint32) int {
 	}
 
 	return bits
+}
+
+// bench 100000000	        3.5 ns/op
+// https://en.wikipedia.org/wiki/Hamming_weight
+func solution4(num uint32) int {
+	num = (num & m1) + (num >> 1) & m1		// 计算2位上1的个数
+	num = (num & m2) + (num >> 2) & m2		// 计算4位上1的个数
+	num = (num & m4) + (num >> 4) & m4		// 计算8位上1的个数
+	num = (num & m8) + (num >> 8) & m8		// 计算16位上1的个数
+	num = (num & m16) + (num >> 16) & m16		// 计算32位上1的个数
+
+	return int(num)
 }
--- leetcode/0191-number-of-1-bits/number-of-1-bits_test.go
+++ leetcode/0191-number-of-1-bits/number-of-1-bits_test.go
@@ -6,27 +6,37 @@ import (
 	"testing"
 )
 
+var tcs = []struct {
+	input uint32
+	ans int
+}{
+	{1, 1},
+	{2, 1},
+	{11, 3},
+	{128, 1},
+	{8, 1},
+	{3, 2},
+}
+
 func TestHammingWeight(t *testing.T) {
 	var i uint32 = 8
 	fmt.Println(i<<1)
 
 	ast := assert.New(t)
 
-	tcs := []struct {
-		input uint32
-		ans int
-	}{
-		{1, 1},
-		{2, 1},
-		{11, 3},
-		{128, 1},
-		{8, 1},
-		{3, 2},
-	}
+
 
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 
 		ast.Equal(tc.ans, hammingWeight(tc.input),"输入:%v", tc)
+	}
+}
+
+func Benchmark_hammingWeight(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			hammingWeight(tc.input)
+		}
 	}
 }
