8399a7c2946f35e1a52348f624eac4ced2518ea6
allbuleyu <allbuleyu@163.com>
2019-03-25 17:51:52 +0800

    使用回溯法解决 prob39

--- leetcode/0039-combination-sum/combination-sum.go
+++ leetcode/0039-combination-sum/combination-sum.go
@@ -1,1 +1,47 @@
 package prob0039
+
+import "sort"
+
+func combinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	res := make([][]int, 0)
+	cur := make([]int, 0)
+
+	helperOptimize(&res, &cur, candidates, target, 0)
+	return  res
+}
+
+func helper(res *[][]int, cur, candidates []int, target, start int) {
+	if target < 0 {
+		return
+	}
+
+	if target == 0 {
+		*res = append(*res, cur)
+	}else {
+		for i := start; i < len(candidates) && candidates[i] <= target; i++ {
+			cur = append(cur, candidates[i])
+			helper(res, cur, candidates, target-candidates[i], i)
+			cur = cur[:len(cur)-1:len(cur)-1]	// 把底层数组的容量也改变,要不然会有错误答案
+		}
+	}
+}
+
+// 优化helper方法,使用的内存更少了
+func helperOptimize(res *[][]int, cur *[]int, candidates []int, target, start int) {
+	if target < 0 {
+		return
+	}
+
+	if target == 0 {
+		c := make([]int, len(*cur))
+		copy(c, *cur)
+		*res = append(*res, c)
+	}else {
+		for i := start; i < len(candidates) && candidates[i] <= target; i++ {
+			*cur = append(*cur, candidates[i])
+			helperOptimize(res, cur, candidates, target-candidates[i], i)
+			*cur = (*cur)[:len(*cur)-1]
+		}
+	}
+}
--- leetcode/0039-combination-sum/combination-sum_test.go
+++ leetcode/0039-combination-sum/combination-sum_test.go
@@ -1,1 +1,43 @@
 package prob0039
+
+import (
+	"testing"
+
+	"fmt"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_prb0039(t *testing.T)  {
+	ast := assert.New(t)
+
+	// test case
+	tcs := []struct{
+		target int
+		input []int
+		ans [][]int
+	}{
+		{
+			7,
+			[]int{2,3,6,7},
+			[][]int{
+				[]int{2,2,3},
+				[]int{7},
+			},
+		},
+		{
+			8,
+			[]int{2,3,5},
+			[][]int{
+				[]int{2,2,2,2},
+				[]int{2,3,3},
+				[]int{3,5},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, combinationSum(tc.input, tc.target), "输入:%v", tc)
+	}
+}
