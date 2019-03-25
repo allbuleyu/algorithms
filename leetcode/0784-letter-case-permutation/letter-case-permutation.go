6ede33578c99be8f409e73448aa1c0ac7b7d94d0
Yuli Huang <allbuleyu@163.com>
2019-03-25 22:50:10 +0800

    prob 784还有点问题

--- leetcode/0784-letter-case-permutation/letter-case-permutation.go
+++ leetcode/0784-letter-case-permutation/letter-case-permutation.go
@@ -2,8 +2,31 @@ package prob0784
 
 func letterCasePermutation(S string) []string {
 	res := make([]string, 0, len(S))
+	cur := make([]byte, 0)
 
-
+	backtrack(&res, &cur, S, 0)
 
 	return res
+}
+
+func backtrack(res *[]string, cur *[]byte, s string, start int) {
+	for i := start; i < len(s); i++ {
+		if s[i] >= 'a' && s[i] <= 'z' {
+			*cur = append(*cur, s[i])
+			backtrack(res, cur, s, i+1)
+			*cur = (*cur)[:i]
+
+			*cur = append(*cur, (s[i]+'A'-'a'))
+			backtrack(res, cur, s, i+1)
+			*cur = (*cur)[:i]
+		}else {
+			*cur = append(*cur, s[i])
+		}
+
+		if len(*cur) == len(s) {
+			*res = append(*res, string(*cur))
+			return
+		}
+	}
+
 }
