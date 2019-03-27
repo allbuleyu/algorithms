370fa7e10a577e19e21d6c5944f40f3b456a4ca8
allbuleyu <allbuleyu@163.com>
2019-03-27 16:15:16 +0800

    新增位操作的方式解决问题,把递归改为迭代还不是很懂

--- leetcode/0078-subsets/subsets.go
+++ leetcode/0078-subsets/subsets.go
@@ -1,13 +1,16 @@
 package prob0078
 
 func subsets(nums []int) [][]int {
-	res := make([][]int, 0)
-	cur := make([]int, 0)
+	//res := make([][]int, 0)
+	//cur := make([]int, 0)
+	//
+	//helper(&res, &cur, nums, 0)
+	//return  res
 
-	helper(&res, &cur, nums, 0)
-	return  res
+	return bitManipulation(nums)
 }
 
+// recursion
 func helper(res *[][]int, cur *[]int, nums []int, start int) {
 
 	c := make([]int, len(*cur))
@@ -19,4 +22,32 @@ func helper(res *[][]int, cur *[]int, nums []int, start int) {
 		helper(res, cur, nums, i+1)
 		*cur = (*cur)[:len(*cur)-1]
 	}
+}
+
+// iterate
+// 迭代方法还没懂
+func helperIterate()  {
+
+}
+
+// 速度比递归稍慢
+func bitManipulation(nums []int) [][]int {
+	n := len(nums)
+	resLen := 1 << uint(n)
+	res := make([][]int, resLen)
+
+	for i := range res {
+		res[i] = make([]int, 0)
+	}
+
+	for i := 0; i < resLen; i++ {
+		for j := 0; j < n; j++ {
+			if i >> uint(j) & 1 == 1 {
+				res[i] = append(res[i], nums[j])
+			}
+
+		}
+	}
+
+	return res
 }
