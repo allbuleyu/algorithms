792aea3b44e0c5042846c46bcb643ad038e84dfa
allbuleyu <allbuleyu@163.com>
2019-05-10 11:38:10 +0800

    增加了广度优先遍历的解法

--- leetcode/0199-binary-tree-right-side-view/binary-tree-right-side-view.go
+++ leetcode/0199-binary-tree-right-side-view/binary-tree-right-side-view.go
@@ -13,7 +13,7 @@ type TreeNode = kit.TreeNode
  * }
  */
 func rightSideView(root *TreeNode) []int {
-	return dfsIterate(root)
+	return bfsIterate(root)
 }
 
 func dfsIterate(root *TreeNode) []int {
@@ -21,7 +21,7 @@ func dfsIterate(root *TreeNode) []int {
 		return nil
 	}
 
-	m := make(map[int]int)
+	m := make(map[int]int)			// 用来判断和保存每一层最右边的节点
 	nodeStack := make([]*TreeNode,0)
 	depthStack := make([]int, 0)
 
@@ -55,8 +55,46 @@ func dfsIterate(root *TreeNode) []int {
 	return res
 }
 
+
+// 广度优先使用一个队列来当辅助
 func bfsIterate(root *TreeNode) []int {
-	return nil
+	if root == nil {
+		return nil
+	}
+	nodeQueue := make([]*TreeNode, 0)
+	depthQueue := make([]int,0)
+	nodeQueue = append(nodeQueue,root)
+	depthQueue = append(depthQueue,0)
+
+	m := make(map[int]int)
+	max_depth := 0
+
+	for len(nodeQueue) != 0 {
+		node := nodeQueue[0]
+		nodeQueue = nodeQueue[1:]
+
+		depth := depthQueue[0]
+		max_depth = max(depth, max_depth)
+		depthQueue = depthQueue[1:]
+
+		if node == nil {
+			continue
+		}
+
+		if _, ok := m[max_depth]; !ok {
+			m[max_depth] = node.Val
+		}
+
+		nodeQueue = append(nodeQueue, node.Right, node.Left)
+		depthQueue = append(depthQueue, depth+1, depth+1)
+	}
+
+	res := make([]int, len(m))
+	for i := 0; i < len(m); i++ {
+		res[i] = m[i]
+	}
+
+	return res
 }
 
 func max(a, b int) int {
