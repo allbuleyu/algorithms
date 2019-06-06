4d8ad27b62d9b0f5d527bbf25a71da3423468ee6
allbuleyu <allbuleyu@163.com>
2019-06-06 18:00:42 +0800

    704 not done!!!!!

--- leetcode/0704-binary-search/binary-search.go
+++ leetcode/0704-binary-search/binary-search.go
@@ -1,7 +1,8 @@
 package prob0704
 
 func search(nums []int, target int) int {
-	return recursionHelp(nums, target, 0, len(nums)-1)
+	return iterateHelp(nums, target)
+	//return recursionHelp(nums, target, 0, len(nums)-1)
 }
 
 func recursionHelp(nums []int, target, left, right int) int {
@@ -14,9 +15,31 @@ func recursionHelp(nums []int, target, left, right int) int {
 		return -1
 	}
 
-	if nums[middle] < target {
-		return recursionHelp(nums, target, middle, right)
+	if nums[middle] > target {
+		return recursionHelp(nums, target, left,middle)
 	}else {
-		return recursionHelp(nums, target, left, middle)
+		return recursionHelp(nums, target, left,middle)
 	}
+}
+
+func iterateHelp(nums []int, target int) int {
+	n := len(nums)
+	pivot := 0
+	left := 0
+	right := n-1
+	for left <= right {
+		pivot = (left + right) / 2
+
+		if nums[pivot] == target {
+			return pivot
+		}
+
+		if nums[pivot] < target {
+			right = pivot - 1
+		}else {
+			left = pivot+1
+		}
+	}
+
+	return -1
 }
--- leetcode/0704-binary-search/binary-search_test.go
+++ leetcode/0704-binary-search/binary-search_test.go
@@ -1,1 +1,31 @@
 package prob0704
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_search(t *testing.T)  {
+	ast := assert.New(t)
+
+	// test case
+	tcs := []struct{
+		input []int
+		target int
+		ans int
+	}{
+		{[]int{-1,0,3,5,9,12}, 9, 4,},
+		{[]int{-1,0,3,5,9,12}, 2, -1,},
+		{[]int{1}, 1, 0,},
+		{[]int{1}, 2, -1,},
+		{[]int{5}, -5, -1,},
+
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, search(tc.input, tc.target), "输入:%v", tc)
+	}
+}
