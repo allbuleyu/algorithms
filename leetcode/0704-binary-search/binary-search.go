424bbfb458eed113d2b812c2ee4577ea51583aed
allbuleyu <allbuleyu@163.com>
2019-06-10 11:37:30 +0800

    704 is done

--- leetcode/0704-binary-search/binary-search.go
+++ leetcode/0704-binary-search/binary-search.go
@@ -1,25 +1,25 @@
 package prob0704
 
 func search(nums []int, target int) int {
-	return iterateHelp(nums, target)
-	//return recursionHelp(nums, target, 0, len(nums)-1)
+	//return iterateHelp(nums, target)
+	return recursionHelp(nums, target, 0, len(nums)-1)
 }
 
 func recursionHelp(nums []int, target, left, right int) int {
+	if left > right {
+		return -1
+	}
+
 	middle := (left + right)/2
 	if nums[middle] == target {
 		return middle
 	}
 
-	if left == right {
-		return -1
+	if nums[middle] < target {
+		return recursionHelp(nums, target, middle+1, right)
 	}
 
-	if nums[middle] > target {
-		return recursionHelp(nums, target, left,middle)
-	}else {
-		return recursionHelp(nums, target, left,middle)
-	}
+	return recursionHelp(nums, target, left, middle-1)
 }
 
 func iterateHelp(nums []int, target int) int {
@@ -35,9 +35,9 @@ func iterateHelp(nums []int, target int) int {
 		}
 
 		if nums[pivot] < target {
-			right = pivot - 1
+			left = pivot + 1
 		}else {
-			left = pivot+1
+			right = pivot - 1
 		}
 	}
 
