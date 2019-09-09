cfe7673682bdd084a2295fe94b79722caa14d8da
allbuleyu <allbuleyu@163.com>
2019-09-09 16:30:31 +0800

    移动窗口的思考

--- leetcode/0713-subarray-product-less-than-k/subarray-product-less-than-k.go
+++ leetcode/0713-subarray-product-less-than-k/subarray-product-less-than-k.go
@@ -21,7 +21,40 @@ func slidingWindow(nums []int, k int) int {
 			i++
 		}
 
+		// 最核心的代码,这一步懂了,移动窗口就非常的简单
+		// 思考: 如果要返回的是结果的数组,而不是个数,如何处理?
 		ans += j-i+1
+	}
+
+	return ans
+}
+
+// 思考: 如果要返回的是结果的数组,而不是个数,如何处理?
+func slidingWindowExt(nums []int, k int) [][]int {
+	if k <= 1 {
+		return [][]int{}
+	}
+
+	var i, j, n, product int
+	n = len(nums)
+	ans := make([][]int, 0)
+	product = 1
+
+	for ; j < n; j++ {
+		product *= nums[j]
+		for product >= k {
+			product /= nums[i]
+			i++
+		}
+
+		// i <= j 才是正确的解
+		if i <= j {
+			ans = append(ans, nums[i:j+1])
+		}
+
+		for t := i+1; t <= j; t++ {
+			ans = append(ans, nums[t:j+1])
+		}
 	}
 
 	return ans
--- leetcode/0713-subarray-product-less-than-k/subarray-product-less-than-k_test.go
+++ leetcode/0713-subarray-product-less-than-k/subarray-product-less-than-k_test.go
@@ -26,3 +26,31 @@ func Test_numSubarrayProductLessThanK(t *testing.T)  {
 		ast.Equal(tc.ans, numSubarrayProductLessThanK(tc.input, tc.k), "输入:%v", tc)
 	}
 }
+
+func Test_slidingWindowExt(t *testing.T)  {
+	ast := assert.New(t)
+
+	// test case
+	tcs := []struct{
+		input []int
+		k int
+		ans [][]int
+	}{
+		//[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+		{[]int{10, 5, 2, 6},100,[][]int{
+			[]int{10}, []int{10, 5}, []int{5}, []int{5, 2}, []int{2}, []int{5, 2, 6}, []int{2, 6}, []int{6},
+		}},
+		{[]int{10,1,2}, 10, [][]int{
+			[]int{1},[]int{1,2},[]int{2},
+		}},
+		{[]int{10,1}, 10, [][]int{
+			[]int{1},
+		}},
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, slidingWindowExt(tc.input, tc.k), "输入:%v", tc)
+	}
+}
