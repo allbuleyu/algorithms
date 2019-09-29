c52ddfad8e264c42f4fb3481d5b225a783a8d306
yuli <allbuleyu@163.com>
2019-09-29 14:24:34 +0800

    第三种方法还不是很懂,回头看

--- leetcode/0189-rotate-array/rotate-array.go
+++ leetcode/0189-rotate-array/rotate-array.go
@@ -5,6 +5,47 @@ func rotate(nums []int, k int)  {
 	reverseArr(nums, k)
 }
 
+// public class Solution {
+//    public void rotate(int[] nums, int k) {
+//        k = k % nums.length;
+//        int count = 0;
+//        for (int start = 0; count < nums.length; start++) {
+//            int current = start;
+//            int prev = nums[start];
+//            do {
+//                int next = (current + k) % nums.length;
+//                int temp = nums[next];
+//                nums[next] = prev;
+//                prev = temp;
+//                current = next;
+//                count++;
+//            } while (start != current);
+//        }
+//    }
+//}
+//环状替换
+func cycleReplace(nums []int, k int) {
+	count := 0
+	for start := 0; count  < len(nums); start++ {
+		current := start
+		prev := nums[start]
+
+		for {
+			next := (current + k) % len(nums)
+			temp := nums[next]
+			nums[next] = prev
+			prev = temp
+			current = next
+			count++
+
+			if start == current {
+				break
+			}
+		}
+	}
+}
+
+
 func extraArr(nums []int, k int) {
 	ans := make([]int, 0)
 
