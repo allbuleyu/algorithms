d344368e21e315ae6761e5430eaad34d6c776f2b
yuli <allbuleyu@163.com>
2019-09-29 17:44:04 +0800

    628 还是写了

--- leetcode/0628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.go
+++ leetcode/0628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.go
@@ -1,5 +1,46 @@
 package prob0628
 
+func maximumProduct(nums []int) int {
+	return onePass(nums)
+}
+
 // 一次迭代的思路
 // 找出2个最小的数,找出3个最大的数
 //
+func onePass(nums []int) int {
+	var min1, min2, max1, max2, max3 int
+	max1, max2, max3 = -1001, -1001, -1001
+	min1, min2 = 1001, 1001
+
+	for _, v := range nums {
+		switch {
+		case v > max1:
+			max1, max2, max3 = v, max1, max2
+		case v > max2:
+			max2, max3 = v, max2
+		case v > max3:
+			max3 = v
+
+		}
+
+		switch {
+		case v < min1:
+			min1, min2 = v, min1
+		case v < min2:
+			min2 = v
+		}
+	}
+
+	return max(min1*min2*max1, max1*max2*max3)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+
+
--- leetcode/0628-maximum-product-of-three-numbers/maximum-product-of-three-numbers_test.go
+++ leetcode/0628-maximum-product-of-three-numbers/maximum-product-of-three-numbers_test.go
@@ -1,1 +1,30 @@
 package prob0628
+
+import (
+	"testing"
+
+	"fmt"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_maximumProduct(t *testing.T)  {
+	ast := assert.New(t)
+
+	// test case
+	tcs := []struct{
+		input []int
+		ans int
+	}{
+		{[]int{1,2,3},6},
+		{[]int{3,2,1},6},
+		{[]int{1,2,3,4},24},
+		{[]int{-1,-2,3,4},8},
+
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, maximumProduct(tc.input), "输入:%v", tc)
+	}
+}
