481f4df15a26517e51f88566e4e53c5227ae0d28
yuli <allbuleyu@163.com>
2020-05-19 14:09:04 +0800

    88 beautiful solution well done

--- leetcode/0088-merge-sorted-array/merge-sorted-array.go
+++ leetcode/0088-merge-sorted-array/merge-sorted-array.go
@@ -50,3 +50,26 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
 		len1--
 	}
 }
+
+// 一次循环,in place func
+func helpFunc1(nums1 []int, m int, nums2 []int, n int) {
+	for i := len(nums1) - 1; i >= 0; i-- {
+		if n == 0 {
+			break
+		}
+
+		if m == 0 {
+			nums1[i] = nums2[n-1]
+			n--
+			continue
+		}
+
+		if nums1[m-1] > nums2[n-1] {
+			nums1[i] = nums1[m-1]
+			m--
+		}else {
+			nums1[i] = nums2[n-1]
+			n--
+		}
+	}
+}
