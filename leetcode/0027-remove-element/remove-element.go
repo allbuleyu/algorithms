c492e52b063a7d1564ca57db2338b6a40253607d
yuli <allbuleyu@163.com>
2020-05-26 09:42:55 +0800

    27 has optimize

--- leetcode/0027-remove-element/remove-element.go
+++ leetcode/0027-remove-element/remove-element.go
@@ -18,3 +18,19 @@ func removeElement(nums []int, val int) int {
 	// why not resIndex+1  thin of line 11 resIndex++ step
 	return resIndex
 }
+
+func helpFunc1(nums []int, val int) int {
+	delNum := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			delNum++
+			continue
+		}
+
+		if delNum > 0 {
+			nums[i-delNum] = nums[i]
+		}
+	}
+
+	return len(nums) - delNum
+}
