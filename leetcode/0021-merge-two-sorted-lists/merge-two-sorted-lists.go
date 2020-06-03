7a00acb1038c2d3923aa52dbadcf5d331e4f878c
yuli <allbuleyu@163.com>
2020-06-03 16:45:38 +0800

    0021 add one approach --recursion

--- leetcode/0021-merge-two-sorted-lists/merge-two-sorted-lists.go
+++ leetcode/0021-merge-two-sorted-lists/merge-two-sorted-lists.go
@@ -11,37 +11,49 @@ type ListNode = kit.ListNode
  * }
  */
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	// 有一条链为nil，直接返回另一条链
+	return helpRecursion(l1, l2)
+}
+
+func helpRecursion(l1, l2 *ListNode) *ListNode {
 	if l1 == nil {
 		return l2
 	}
+
 	if l2 == nil {
 		return l1
 	}
 
-	preHead := &ListNode{}
-	cur := preHead
+	if l1.Val < l2.Val {
+		l1.Next = helpRecursion(l1.Next, l2)
+		return l1
+	}
+
+	l2.Next = helpRecursion(l1, l2.Next)
+	return l2
+}
+
+func helpIteration(l1, l2 *ListNode) *ListNode {
+	prev := &ListNode{Next:nil}
+	cur := prev
 	for l1 != nil && l2 != nil {
 		if l1.Val < l2.Val {
 			cur.Next = l1
 			l1 = l1.Next
-			cur = cur.Next
 		}else {
 			cur.Next = l2
 			l2 = l2.Next
-			cur = cur.Next
 		}
-	}
 
-	if l2 != nil {
-		l1 = l2
-	}
-
-	for l1 != nil {
-		cur.Next = l1
-		l1 = l1.Next
 		cur = cur.Next
 	}
 
-	return preHead.Next
+	if l1 == nil {
+		cur.Next = l2
+	}
+
+	if l2 == nil {
+		cur.Next = l1
+	}
+
+	return prev.Next
 }
