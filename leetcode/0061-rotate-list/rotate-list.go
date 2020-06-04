9b067aaa89fb29ffd28a36b36dd810f11213273a
yuli <allbuleyu@163.com>
2020-06-04 17:12:32 +0800

    add one nice approch

--- leetcode/0061-rotate-list/rotate-list.go
+++ leetcode/0061-rotate-list/rotate-list.go
@@ -16,37 +16,67 @@ func rotateRight(head *ListNode, k int) *ListNode {
 		return head
 	}
 
-	return iterate(head, k)
+	return optimize(head, k)
 }
 
-func iterate(head *ListNode, k int) *ListNode {
-	lenList := 1
-	pre := &ListNode{Next:head}
-	cur := head
-
-	for cur.Next != nil {
-		lenList++
-		cur = cur.Next
-	}
-
-	// return origin list
-	if k % lenList == 0 {
+func optimize(head *ListNode, k int) *ListNode {
+	if head == nil || head.Next == nil {
 		return head
 	}
 
-	// rotate list tail
-	tail := cur
+	l := 1
+	cur := head
+	for cur != nil && cur.Next != nil {
+		l++
+		cur = cur.Next
+	}
 
-	// find the rotate position
-	cur = pre
-	for k = lenList-(k%lenList); k > 0; k-- {
+	i := l - k % l
+	if i == l {
+		return head
+	}
+
+	cur.Next = head
+	for ; i > 0; i-- {
 		cur = cur.Next
 	}
 
 	head = cur.Next
 	cur.Next = nil
-	tail.Next=pre.Next
-	pre.Next=head
+	return head
+}
 
-	return pre.Next
+func iterate(head *ListNode, k int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	l := 0
+	first := head
+	for first != nil {
+		l++
+		first = first.Next
+	}
+
+	first = head
+	second := head
+	i := l - k % l
+	if i == l {
+		return head
+	}
+
+	for ; i > 1; i-- {
+		first = first.Next
+	}
+
+	head = first.Next
+	// first.Next 是新的头部,那么first就是新的尾部
+	first, first.Next = first.Next, nil
+	for first != nil && first.Next != nil {
+		first = first.Next
+	}
+
+	first.Next = second
+
+	return head
 }
