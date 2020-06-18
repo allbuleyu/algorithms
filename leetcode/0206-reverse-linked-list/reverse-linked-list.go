742ad13ca3477e44b90b00263dd81cd8e50af801
yuli <allbuleyu@163.com>
2020-06-18 10:36:32 +0800

    用递归的方式解决206

--- leetcode/0206-reverse-linked-list/reverse-linked-list.go
+++ leetcode/0206-reverse-linked-list/reverse-linked-list.go
@@ -11,6 +11,12 @@ type ListNode = kit.ListNode
  * }
  */
 func reverseList(head *ListNode) *ListNode {
+	dummy := &ListNode{}
+	helpRecursionDownUp(head, dummy)
+	return dummy.Next
+}
+
+func helpIteration(head *ListNode) *ListNode {
 	dummy := &ListNode{Next:nil}
 
 	for head != nil {
@@ -21,4 +27,28 @@ func reverseList(head *ListNode) *ListNode {
 	}
 
 	return dummy.Next
+}
+
+func helpRecursionDownUp(head, newHead *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		newHead.Next = head
+		return head
+	}
+
+	next := helpRecursionDownUp(head.Next, newHead)
+	next.Next = head
+	head.Next = nil
+	return head
+}
+
+func helpRecursionUpDown(head, dummy *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	next := head.Next
+	head.Next = dummy.Next
+	dummy.Next = head
+
+	return helpRecursionUpDown(next, dummy)
 }
