b455431b1d3c47f4fa4e9281a68f934881a07672
yuli <allbuleyu@163.com>
2020-07-03 17:17:08 +0800

    46 done

--- leetcode/0046-permutations/permutations.go
+++ leetcode/0046-permutations/permutations.go
@@ -14,6 +14,28 @@ func recursion(nums []int) [][]int {
 	return res
 }
 
+func backtrack(nums *[]int, ans *[][]int, n, start int) {
+	if start == n {
+		c := make([]int, n)
+		copy(c, *nums)
+		*ans = append(*ans, c)
+		return
+	}
+
+	for i := start; i < n; i++ {
+		// 这里用swap 比用数组长度的2倍方法机智太多了!!!!
+		// 数组2倍长度维护真的烦, 说的就是helpRecursion这个
+		swap(nums, start, i)
+		backtrack(nums, ans, n, start+1)
+		swap(nums, start, i)
+	}
+}
+
+func swap(nums *[]int, i, j int) {
+	t := *nums
+	t[i], t[j] = t[j], t[i]
+}
+
 // 1,2,3 通过for 和把当前的切片缩短,可以替换1,2,3的出现位置
 func helpRecursion(res *[][]int, nums []int, cur *[]int) {
 	if len(*cur) == len(nums) {
