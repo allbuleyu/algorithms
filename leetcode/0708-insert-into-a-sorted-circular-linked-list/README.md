6fe853d52885d27d772a654b8bb21b5b52503ffa
yuli <allbuleyu@163.com>
2020-07-06 16:52:30 +0800

    done

---
+++ leetcode/0708-insert-into-a-sorted-circular-linked-list/README.md
@@ -0,0 +1,1 @@
+# [0708-insert-into-a-sorted-circular-linked-list](https://leetcode.com/problems/insert-into-a-sorted-circular-linked-list)
---
+++ leetcode/0708-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.go
@@ -0,0 +1,49 @@
+package prob0708
+
+import "github.com/allbuleyu/algorithms/kit"
+
+type Node = kit.ListNode
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ * }
+ */
+
+func insert(aNode *Node, x int) *Node {
+	if aNode == nil {
+		head := &Node{Val:x}
+		head.Next = head
+		return head
+	}
+
+	min := aNode
+	max := aNode
+	cur := aNode
+	for {
+		if cur.Val < min.Val {
+			min = cur
+		}
+
+		if cur.Val >= max.Val {
+			max = cur
+		}
+
+		if cur.Val <= x && cur.Next.Val > x {
+			next := cur.Next
+			cur.Next = &Node{Val:x, Next:next}
+			return aNode
+		}
+
+		if cur.Next == aNode {
+			break
+		}
+		cur = cur.Next
+	}
+
+	next := max.Next
+	max.Next = &Node{Val:x, Next:next}
+
+	return aNode
+}
