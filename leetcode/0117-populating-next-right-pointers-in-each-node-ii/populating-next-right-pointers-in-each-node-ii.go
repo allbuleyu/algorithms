8d270b9b835e6b5e22759fc1c9b3ee1e9d3dd2ce
yuli <allbuleyu@163.com>
2020-07-07 17:54:56 +0800

    117 done

--- leetcode/0117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.go
+++ leetcode/0117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.go
@@ -15,55 +15,46 @@ type Node struct {
  */
 
 func connect(root *Node) *Node {
-	helpTwoPointers(root)
+	helpRecursion(root)
 	return root
 }
 
-func helpTwoPointers(root *Node) {
-	var start, cur *Node
-	start = root
-	for start != nil {
-		cur = start
-		for cur != nil && (cur.Left != nil || cur.Right != nil || cur.Next != nil) {
-			var pre *Node
-			if cur.Left != nil {
-				pre = cur.Left
-				if cur.Right != nil {
-					cur.Left.Next = cur.Right
-					pre = cur.Right
-				}
-
-			}else if cur.Right != nil {
-				pre = cur.Right
-			}else {
-				cur = cur.Next
-				continue
-			}
-
-			if cur.Next != nil {
-				next := cur.Next
-				for next != nil {
-					if next.Left != nil {
-						pre.Next = next.Left
-						break
-					}else if next.Right != nil {
-						pre.Next = next.Right
-						break
-					}
-
-					next = next.Next
-				}
-			}
-
-			cur = cur.Next
-		}
-
-		if start.Left != nil {
-			start = start.Left
-		}else if start.Right != nil {
-			start = start.Right
-		}else {
-			start = start.Next
-		}
+// it is very easy to tranfer iteration
+func helpRecursion(root *Node) {
+	if root == nil {
+		return
 	}
+
+	var child, prev *Node
+	node := root
+	for node != nil {
+		if node.Left == nil && node.Right == nil {
+			node = node.Next
+			continue
+		}
+
+		if node.Left != nil {
+			if child == nil {
+				child = node.Left
+				prev = node.Left
+			}else {
+				prev.Next = node.Left
+				prev = prev.Next
+			}
+		}
+
+		if node.Right != nil {
+			if child == nil {
+				child = node.Right
+				prev = node.Right
+			}else {
+				prev.Next = node.Right
+				prev = prev.Next
+			}
+		}
+
+		node = node.Next
+	}
+
+	helpRecursion(child)
 }
