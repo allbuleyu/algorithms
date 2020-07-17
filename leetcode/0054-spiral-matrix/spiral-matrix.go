2e06004ae1ea9f716e9f8e97329f981e0df39ea9
yuli <allbuleyu@163.com>
2020-07-17 11:06:36 +0800

    done with straight way

--- leetcode/0054-spiral-matrix/spiral-matrix.go
+++ leetcode/0054-spiral-matrix/spiral-matrix.go
@@ -5,13 +5,6 @@ func spiralOrder(matrix [][]int) []int {
 	return iteration(matrix)
 }
 
-// var directions = map[string][]int{
-//     "right":[]int{0, 1},
-//     "down":[]int{1, 0},
-//     "left":[]int{0, -1},
-//     "up":[]int{-1, 0},
-// }
-
 type node struct{
 	val []int
 	size []int	// m,n,mm,mn
@@ -20,26 +13,26 @@ type node struct{
 
 var right = &node{
 	val:[]int{0, 1},
-	size:[]int{0, -1, 0, 0},
+	size:[]int{0, 0, 1, 0},
 	next:down,
 }
 
 var down = &node{
 	val:[]int{1, 0},
-	size:[]int{-1, 0, 0, 0},
+	size:[]int{0, -1, 0, 0},
 	next:left,
 }
 
 var left = &node{
 	val:[]int{0, -1},
-	size:[]int{0, 0, 0, 1},
+	size:[]int{-1, 0, 0, 0},
 	next:up,
 }
 
 var up = &node{
 	val:[]int{-1, 0},
-	size:[]int{0, 0, 1, 0},
-	next:right,
+	size:[]int{0, 0, 0, 1},
+	next:nil,
 }
 
 func iteration(matrix [][]int) []int {
@@ -47,28 +40,67 @@ func iteration(matrix [][]int) []int {
 		return []int{}
 	}
 	m, n := len(matrix), len(matrix[0])
-	mm, mn := 0, 0
-
-	direction := right
-	r, c := 0, 0
+	r1, r2, c1, c2 := 0, m-1, 0, n-1
 	ans := make([]int, 0, m*n)
-	for {
-		x, y := direction.val[0], direction.val[1]
-		for r < m && c < n && r >= mm && c >= mn {
-			ans = append(ans, matrix[r][c])
-			if r == mm || r == m-1 || c == mn || c == n-1 {
-				break
+	for r1 <= r2 && c1 <= c2 {
+		// direction right
+		for c := c1; c <= c2; c++ {
+			ans = append(ans, matrix[r1][c])
+		}
+
+		// direction down
+		for r := r1+1; r <= r2; r++ {
+			ans = append(ans, matrix[r][c2])
+		}
+
+		// 这里把单行,单列的排除
+		if r1 < r2 && c1 < c2 {
+			// direction left
+			for c := c2-1; c >= c1; c-- {
+				ans = append(ans, matrix[r2][c])
 			}
-			r, c = r+x, c+y
+
+			// direction up
+			for r := r2-1; r >= r1+1; r-- {
+				ans = append(ans, matrix[r][c1])
+			}
 		}
 
-		m, n, mm, mn = m+direction.val[0], n+direction.val[1],mm+direction.val[2], mn+direction.val[3]
-		direction = direction.next
-
-		if mm == m-1 {
-			break
-		}
+		r1++
+		c1++
+		r2--
+		c2--
 	}
 
 	return ans
 }
+
+//func iteration(matrix [][]int) []int {
+//	if len(matrix) == 0 || len(matrix[0]) == 0 {
+//		return []int{}
+//	}
+//	m, n := len(matrix), len(matrix[0])
+//	mm, mn := 0, 0
+//
+//	direction := right
+//	up.next=right
+//	r, c := 0, 0
+//	ans := make([]int, 0, m*n)
+//	for {
+//		x, y := direction.val[0], direction.val[1]
+//		for r < m && c < n && r >= mm && c >= mn {
+//			ans = append(ans, matrix[r][c])
+//			r, c = r+x, c+y
+//		}
+//
+//
+//		m, n, mm, mn = m+direction.size[0], n+direction.size[1],mm+direction.size[2], mn+direction.size[3]
+//		direction = direction.next
+//
+//		if mm == m {
+//			break
+//		}
+//	}
+//
+//	return ans
+//}
--- leetcode/0054-spiral-matrix/spiral-matrix_test.go
+++ leetcode/0054-spiral-matrix/spiral-matrix_test.go
@@ -1,1 +1,50 @@
 package prob0054
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_spiralOrder(t *testing.T) {
+
+	ast := assert.New(t)
+
+	tcs := []struct{
+		input [][]int
+		ans []int
+	}{
+		{
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{4, 5, 6},
+				[]int{7, 8, 9},
+			},[]int{1,2,3,6,9,8,7,4,5},
+		},
+		{
+			[][]int{
+				[]int{1, 2, 3, 4},
+			},[]int{1,2,3,4},
+		},
+		{
+			[][]int{
+				[]int{1, 2, 3},
+				[]int{7, 8, 9},
+			},[]int{1,2,3,9,8,7},
+		},
+		{
+			[][]int{
+				[]int{1},
+				[]int{2},
+				[]int{3},
+			},[]int{1,2,3},
+		},
+
+	}
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		ast.Equal(tc.ans, spiralOrder(tc.input), "输入:%v", tc)
+	}
+}
