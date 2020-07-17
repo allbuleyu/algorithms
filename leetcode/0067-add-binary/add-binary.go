be63f74b3a13dc5b3f7e1fd8216d5651cbb8d370
yuli <allbuleyu@163.com>
2020-07-17 18:01:59 +0800

    bit by bit solution

--- leetcode/0067-add-binary/add-binary.go
+++ leetcode/0067-add-binary/add-binary.go
@@ -1,61 +1,66 @@
 package prob0067
 
-import "strings"
+import "strconv"
 
 func addBinary(a string, b string) string {
-	na, nb := len(a), len(b)
 
-	if nb > na {
-		a, b = b, a
-		na, nb = nb, na
+	return bitManipulation(a, b)
+}
+
+// 位操作还是有问题
+func bitManipulation(a, b string) string {
+	m, n := len(a), len(b)
+	if n > m {
+		return bitManipulation(b, a)
+	}
+	ba, _ := strconv.ParseInt(a, 2, 64)
+	bb, _ := strconv.ParseInt(b, 2, 64)
+
+	for bb > 0 {
+		carry := (ba & bb) << 1
+		ba = ba ^ bb
+		bb = carry
 	}
 
+	return strconv.FormatInt(ba, 2)
+}
 
-	s := make([]string, na+1)
-	var carry byte
-	carry = '0'
-	for na > 0 {
-
-		var current string
-
-		if nb > 0 {
-			if a[na-1] + b[nb-1] + carry == '1' * 3 {
-				carry = '1'
-				current = "1"
-			}else if a[na-1] + b[nb-1] + carry == (('1'*2) +'0') {
-				carry = '1'
-				current = "0"
-			}else if a[na-1] + b[nb-1] + carry == (('1') +'0' * 2) {
-				carry = '0'
-				current = "1"
-			}else {
-				carry = '0'
-				current = "0"
-			}
-			nb--
-		}else {
-			if a[na-1] + carry == '1' * 2 {
-				carry = '1'
-				current = "0"
-			}else if a[na-1] + carry == '1' + '0' {
-				carry = '0'
-				current = "1"
-			}else if a[na-1] + carry == '0' * 2 {
-				carry = '0'
-				current = "0"
-			}
+func bitByBit(a, b string) string {
+	m, n := len(a), len(b)
+	if n > m {
+		return bitByBit(b, a)
+	}
+	ans := make([]byte, 0, m+1)
+	carry := 0
+	for i, j := m-1, n-1; i >= 0; {
+		if a[i] == '1' {
+			carry++
+		}
+		if j >= 0 && b[j] == '1' {
+			carry++
 		}
 
-		s[na] = current
-		na--
+		if carry % 2 != 0 {
+			ans = append(ans, '1')
+		}else {
+			ans = append(ans, '0')
+		}
 
+		carry /= 2
+		i--
+		j--
 	}
 
-	if carry == '1' {
-		s[0] = "1"
-
-		return strings.Join(s, "")
+	if carry == 1 {
+		ans = append(ans, '1')
 	}
 
-	return strings.Join(s[1:], "")
+	reverse(ans)
+	return string(ans)
+}
+
+func reverse(b []byte) {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
 }
