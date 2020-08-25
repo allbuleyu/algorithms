f7a1636dc6b983319c5d8bc7e31032fe22daf7b0
yuli <allbuleyu@163.com>
2020-08-25 09:53:59 +0800

    optimize mono stack

--- leetcode/0739-daily-temperatures/daily-temperatures.go
+++ leetcode/0739-daily-temperatures/daily-temperatures.go
@@ -23,26 +23,16 @@ func dailyTemperatures(T []int) []int {
 
 func helpMonoStack(t []int) []int {
 	stack := make([]int, 0)
-	hs := make(map[int]int)
+	ans := make([]int, len(t))
 
 	for i := 0; i < len(t); i++ {
 		for len(stack) > 0 && t[stack[len(stack)-1]] < t[i] {
 			m := len(stack)
-			hs[stack[m-1]] = i
+			ans[stack[m-1]] = i - stack[m-1]
 			stack = stack[:m-1:cap(stack)]
 		}
 
 		stack = append(stack, i)
-	}
-
-	ans := make([]int, len(t))
-	for i := 0; i < len(t); i++ {
-		k, ok := hs[i]
-		if !ok {
-			ans[i] = 0
-		}else {
-			ans[i] = k-i
-		}
 	}
 
 	return ans
