8eff8f0adadc3c3f10f60a97fd3e81a40f3befb7
huangyuli <allbuleyu@163.com>
2022-12-26 14:46:02 +0800

    整理package

--- main.go
+++ main.go
@@ -1,94 +1,96 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os/exec"
+	"strings"
+	"bytes"
+	"os"
 )
 
-func main() {
-	in := []string{"ab", "ba", "aaab", "abab", "baa"}
-	s := Constructor(in)
-	b := []byte{'a', 'a', 'a'}
-	for i := 0; i < len(b); i++ {
-		fmt.Println(s.Query(b[i]))
-	}
-	fmt.Println(s)
-}
+var (
+	pkg = flag.String("p", "0", "pkg name:example -p=0001 package = prob0001")
+	name = flag.String("m", "", "file name")
 
-type StreamChecker struct {
-	in   []byte
-	root *tree
-}
+	// 主目录
+	base = "./leetcode"
+)
 
-type tree struct {
-	c        byte
-	w        bool
-	children []*tree
-}
+func main()  {
+	flag.Parse()
 
-func Constructor(words []string) StreamChecker {
-	root := &tree{}
-	for _, word := range words {
-		root.insert(word)
+	if *pkg == "0" {
+		fmt.Println("pkg name can not be 0 or empty")
+		return
 	}
 
-	return StreamChecker{in: make([]byte, 0), root: root}
-}
-
-func (root *tree) insert(word string) {
-	t := root
-	for i := len(word) - 1; i >= 0; i-- {
-		if t.children == nil {
-			t.children = make([]*tree, 0)
-		}
-
-		k := -1
-		for j := 0; j < len(t.children); j++ {
-			if t.children[j].c == word[i] {
-				k = j
-			}
-		}
-
-		if k == -1 {
-			t.children = append(t.children, &tree{c: word[i]})
-			k = len(t.children) - 1
-		}
-
-		t = t.children[k]
+	if strings.Trim(*name, " ") == "" {
+		fmt.Println("name can not empty")
+		return
 	}
 
-	t.w = true
-}
+	*pkg = strings.Replace(fmt.Sprintf("%4s", *pkg), " ", "0", 4)
 
-func (root *tree) search(in []byte) bool {
-	t := root
-	for i := len(in) - 1; i >= 0; i-- {
-		k := -1
-		for j := 0; j < len(t.children); j++ {
-			if t.children[j].c == in[i] {
-				k = j
-			}
-		}
+	buf := bytes.Buffer{}
+	buf.WriteString(*pkg)
+	buf.WriteByte('-')
+	buf.WriteString(*name)
 
-		if k == -1 {
-			return false
-		}
+	dname := buf.String()
 
-		t = t.children[k]
-		if t.w {
-			return true
-		}
+	dpath := fmt.Sprintf("%s/%s", base, dname)
+	err := os.Mkdir(dpath, 0766)
+	//if  {
+	//	fmt.Printf("%s dir is exist! \n", dpath)
+	//	return
+	//}
+
+	isExist := os.IsExist(err)
+	if isExist {
+		panic("file or dir has been existed")
 	}
 
-	return false
-}
+	if err != nil && !isExist {
+		fmt.Printf("%s \n", err)
+		return
+	}
 
-func (sc *StreamChecker) Query(letter byte) bool {
-	sc.in = append(sc.in, letter)
-	return sc.root.search(sc.in)
-}
+	pkgStr := fmt.Sprintf("package prob%s",*pkg)
+	fpath := fmt.Sprintf("%s/%s.go", dpath, *name)
+	ftPath :=fmt.Sprintf("%s/%s_test.go", dpath, *name)
+	f, err := os.Create(fpath)
+	if err != nil  {
+		fmt.Printf("mkdir err is %s \n", err)
+		return
+	}
 
-/**
- * Your StreamChecker object will be instantiated and called as such:
- * obj := Constructor(words);
- * param_1 := obj.Query(letter);
- */
+	ft, err := os.Create(ftPath)
+	if err != nil  {
+		fmt.Printf("mkdir err is %s \n", err)
+		return
+	}
+
+	f.WriteString(pkgStr)
+	ft.WriteString(pkgStr)
+
+	mdPath := fmt.Sprintf("%s/README.md", dpath)
+	fmd, err := os.Create(mdPath)
+	fmd.WriteString(fmt.Sprintf("# [%s-%s](https://leetcode.com/problems/%s)", *pkg, *name, *name))
+
+	// 运行Git命令,把生成的文件加入Git中
+	cmd := exec.Command("git", "add", dpath)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("cmd run fail, pls check err")
+		return
+	}
+
+
+
+	defer fmd.Close()
+	defer ft.Close()
+	defer f.Close()
+
+	fmt.Println(*pkg, *name, dname)
+}
