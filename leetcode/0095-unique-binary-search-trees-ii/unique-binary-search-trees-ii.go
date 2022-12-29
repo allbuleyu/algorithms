9736e81720ca6159c83e4621cab75e19b1446a05
huangyuli <allbuleyu@163.com>
2022-12-29 23:12:25 +0800

    95 undone

--- leetcode/0095-unique-binary-search-trees-ii/unique-binary-search-trees-ii.go
+++ leetcode/0095-unique-binary-search-trees-ii/unique-binary-search-trees-ii.go
@@ -1,1 +1,39 @@
 package prob0095
+
+import "algorithms/kit"
+
+type TreeNode = kit.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+	return help(0, n)
+}
+
+func help(start, end int) []*TreeNode {
+	if start > end {
+		return nil
+	}
+
+	nodes := make([]*TreeNode, 0)
+	for i := start; i <= end; i++ {
+		left := help(start, i-1)
+		right := help(i+1, end)
+
+		node := &TreeNode{Val: i}
+
+		if len(left) == 0 && len(right) == 0 {
+			nodes = append(nodes, node)
+			return nodes
+		}
+
+	}
+
+	return nodes
+}
