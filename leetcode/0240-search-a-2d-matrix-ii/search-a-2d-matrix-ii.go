f6b046f62ca6a8a03b7e16a9376ad7835508e1dd
huangyuli <allbuleyu@163.com>
2023-01-03 19:59:36 +0800

    dacDfsBinarySearch do not done

--- leetcode/0240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
+++ leetcode/0240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -17,16 +17,15 @@ func helpZigzag(matrix [][]int, target int) bool {
 	for row >= 0 && col < n {
 		if matrix[row][col] == target {
 			return true
-		}else if matrix[row][col] < target {
+		} else if matrix[row][col] < target {
 			col++
-		}else {
+		} else {
 			row--
 		}
 	}
 
 	return false
 }
-
 
 // 可以把矩阵划分为4个子矩阵
 // 两个矩阵可能包含target, 2个矩阵则绝对不包含target
@@ -36,10 +35,10 @@ func helpZigzag(matrix [][]int, target int) bool {
 func divideAndConquer(martrix *[][]int, target, up, down, left, right int) bool {
 	if up > down || left > right {
 		return false
-	}else if target < (*martrix)[up][left] || target > (*martrix)[down][right] {
+	} else if target < (*martrix)[up][left] || target > (*martrix)[down][right] {
 		return false
 	}
-	
+
 	mid := left + (right-left)/2
 
 	row := up
@@ -53,3 +52,48 @@ func divideAndConquer(martrix *[][]int, target, up, down, left, right int) bool 
 	return divideAndConquer(martrix, target, up, down, left, mid-1) || divideAndConquer(martrix, target, up, row-1, left+1, right)
 }
 
+const used = -1e9 - 1
+
+func helpDacDfs(matrix [][]int, target int) bool {
+	return dacDfs(matrix, target, 0, 0, len(matrix), len(matrix[0]))
+}
+
+func dacDfs(matrix [][]int, target, i, j, m, n int) bool {
+	if i >= m || j >= n || matrix[i][j] == used {
+		return false
+	}
+
+	if matrix[i][j] == target {
+		return true
+	}
+	matrix[i][j] = used
+
+	return dacDfs(matrix, target, i+1, j, m, n) || dacDfs(matrix, target, i, j+1, m, n)
+}
+
+func helpDacDfsBinarySearch(matrix [][]int, target int) bool {
+	return dacDfsBinarySearch(matrix, target, 0, len(matrix)-1, 0, len(matrix[0])-1)
+}
+
+func dacDfsBinarySearch(matrix [][]int, target, up, down, left, right int) bool {
+	if up < 0 || up > down || left < 0 || left > right {
+		return false
+	}
+
+	hMid, vMid := (up+down)/2, (left+right)/2
+	if matrix[hMid][vMid] == used {
+		return false
+	}
+
+	if matrix[hMid][vMid] == target {
+		return true
+	}
+
+	matrix[hMid][vMid] = used
+
+	if matrix[hMid][vMid] < target {
+		return dacDfsBinarySearch(matrix, target, hMid+1, down, left, right) || dacDfsBinarySearch(matrix, target, up, down, vMid+1, right)
+	}
+
+	return dacDfsBinarySearch(matrix, target, up, hMid, left, right) || dacDfsBinarySearch(matrix, target, up, down, left, vMid)
+}
