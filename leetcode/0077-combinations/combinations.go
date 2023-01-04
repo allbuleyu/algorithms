560cbe404ecf453f83a7dc43a9a44d301d4dae73
huangyuli <allbuleyu@163.com>
2023-01-04 11:41:35 +0800

    combinations

--- leetcode/0077-combinations/combinations.go
+++ leetcode/0077-combinations/combinations.go
@@ -1,26 +1,26 @@
 package prob0077
 
+var combinations [][]int
+var cache []int
 
 func combine(n int, k int) [][]int {
-
-	ans := make([][]int, 0)
-	cur := make([]int, 0)
-	backtrack(n,k, 1, &ans, &cur)
-	return ans
+	combinations = make([][]int, 0)
+	cache = make([]int, 0)
+	backtrack(n, k, 1)
+	return combinations
 }
 
-func backtrack(n, k, start int, ans *[][]int, cur *[]int) {
-	if k == 0 {
-		c := make([]int, len(*cur))
-		copy(c, *cur)
-		*ans = append(*ans, c)
-
+func backtrack(n, k, begin int) {
+	if len(cache) == k {
+		cur := make([]int, k)
+		copy(cur, cache)
+		combinations = append(combinations, cur)
 		return
 	}
 
-	for i := start; i <= n+1-k; i++ {
-		*cur = append(*cur, i)
-		backtrack(n, k-1, i+1, ans, cur)
-		*cur = (*cur)[:len(*cur)-1]
+	for i := begin; i <= n; i++ {
+		cache = append(cache, i)
+		backtrack(n, k, i+1)
+		cache = cache[:len(cache)-1]
 	}
 }
