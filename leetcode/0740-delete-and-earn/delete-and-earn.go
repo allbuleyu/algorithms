5796a28f63c7855557ba969f9e06a0392aa5db55
huangyuli <allbuleyu@163.com>
2023-01-14 18:14:09 +0800

    740

--- leetcode/0740-delete-and-earn/delete-and-earn.go
+++ leetcode/0740-delete-and-earn/delete-and-earn.go
@@ -1,5 +1,67 @@
 package prob0740
 
+import "sort"
+
 func deleteAndEarn(nums []int) int {
-	return 0
+	return dpBottomUp(nums)
+}
+
+// worst case time complexity O(x^2).
+func f1(nums []int) int {
+	m := make(map[int]int)
+	for i := range nums {
+		m[nums[i]] += nums[i]
+	}
+
+	res := 0
+	for i := range nums {
+		x, y := nums[i]-1, nums[i]+1
+
+		sum := 0
+		for k, v := range m {
+			if k != x && k != y {
+				sum += v
+			}
+		}
+
+		res = max(res, sum)
+	}
+
+	return res
+}
+
+func dpBottomUp(nums []int) int {
+	sort.Ints(nums)
+
+	m := make(map[int]int)
+	for _, v := range nums {
+		m[v] += v
+	}
+
+	var x, y int
+	for i := 0; i < len(nums); i++ {
+		for i > 0 && i < len(nums) && nums[i] == nums[i-1] {
+			i++
+		}
+
+		if i == len(nums) {
+			break
+		}
+
+		if _, ok := m[nums[i]-1]; !ok {
+			x, y = x+m[nums[i]], x
+		} else {
+			x, y = max(x, y+m[nums[i]]), x
+		}
+	}
+
+	return x
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
 }
--- leetcode/0740-delete-and-earn/delete-and-earn_test.go
+++ leetcode/0740-delete-and-earn/delete-and-earn_test.go
@@ -1,1 +1,22 @@
 package prob0740
+
+import "testing"
+
+func Test_deleteAndEarn(t *testing.T) {
+
+	tests := []struct {
+		name string
+		args []int
+		want int
+	}{
+		{"1", []int{1, 2}, 2},
+		{"1x", []int{3, 4, 2}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteAndEarn(tt.args); got != tt.want {
+				t.Errorf("deleteAndEarn() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
