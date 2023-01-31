88852e46b403bb255cb498584f8a89fab9671da5
huangyuli <allbuleyu@163.com>
2023-01-31 11:46:07 +0800

    dp done 53

--- leetcode/0053-maximum-subarray/maximum-subarray.go
+++ leetcode/0053-maximum-subarray/maximum-subarray.go
@@ -10,7 +10,38 @@ func maxSubArray(nums []int) int {
 	}
 
 	//return divideAndConquer(nums, 0, len(nums)-1)
-	return dynamicPrograming(nums)
+	return bottomUp(nums)
+}
+
+func bottomUp(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	res := nums[0]
+	sum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		sum = max(sum+nums[i], nums[i], nums[i])
+		if res < sum {
+			res = sum
+		}
+	}
+
+	return res
+}
+
+// f(i) = max(f(i-1)+nums[i], nums[i])
+func topDown(nums []int, start int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	if start == 0 {
+		return nums[0]
+	}
+
+	res := max(topDown(nums, start-1)+nums[start], nums[start], nums[start])
+	return res
 }
 
 func regular(nums []int) int {
@@ -54,28 +85,28 @@ func dynamicPrograming(nums []int) int {
 }
 
 // 分治法
-func divideAndConquer(nums []int, left,right int) int {
+func divideAndConquer(nums []int, left, right int) int {
 	if left == right {
 		return nums[left]
 	}
 
 	var leftMaxSum, rightMaxSum, middleMaxSum int
 
-	middle := (right+left)/2
+	middle := (right + left) / 2
 
 	leftMaxSum = divideAndConquer(nums, left, middle)
 	rightMaxSum = divideAndConquer(nums, middle+1, right)
 
 	var leftSubMaxSum, rightSubMaxSum int = int(math.MinInt32), int(math.MinInt32)
 	var leftSubSum, rightSubSum int
-	for i := middle;i>=left;i-- {
+	for i := middle; i >= left; i-- {
 		leftSubSum += nums[i]
 		if leftSubMaxSum < leftSubSum {
 			leftSubMaxSum = leftSubSum
 		}
 	}
 
-	for i := middle+1; i <= right; i++ {
+	for i := middle + 1; i <= right; i++ {
 		rightSubSum += nums[i]
 		if rightSubMaxSum < rightSubSum {
 			rightSubMaxSum = rightSubSum
---
+++ leetcode/0918-maximum-sum-circular-subarray/README.md
@@ -0,0 +1,1 @@
+# [0918-maximum-sum-circular-subarray](https://leetcode.com/problems/maximum-sum-circular-subarray)
