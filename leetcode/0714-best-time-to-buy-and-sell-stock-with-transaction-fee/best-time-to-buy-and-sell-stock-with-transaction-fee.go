aad00bf080466afb71238276b9b6cd15fe3432ba
huangyuli <allbuleyu@163.com>
2023-02-04 10:16:29 +0800

    dp 714 done

--- leetcode/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.go
+++ leetcode/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.go
@@ -1,18 +1,53 @@
 package prob0714
 
 func maxProfit(prices []int, fee int) int {
-	var none, buy, sell int
-	buy = prices[0]
-	// todo: 状态转变回去再写
-	for i := 1; i < len(prices); i++ {
-		none1 := max(none, max(sell, buy+prices[i]))
-		buy1 := max(-prices[i]+none, max(buy, -prices[i]+sell))
-		sell = buy + prices[i]
+	return bottomUp(prices, fee)
+}
 
-		none, buy = none1, buy1
+func bottomUp(prices []int, fee int) int {
+	var buy, sell int
+	buy = -prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		sell = max(sell, buy+prices[i]-fee)
+		buy = max(buy, -prices[i]+sell)
 	}
 
 	return sell
+}
+
+var memory [][]int
+
+func helper(prices []int, fee int) int {
+	memory = make([][]int, 2)
+
+	for i := 0; i < 2; i++ {
+		memory[i] = make([]int, len(prices))
+		for j := 0; j < len(prices); j++ {
+			memory[i][j] = -1
+		}
+	}
+	return upDown(prices, fee, 0, 0)
+}
+
+func upDown(prices []int, fee, x, y int) int {
+	if x == len(prices) {
+		return 0
+	}
+
+	if memory[y][x] != -1 {
+		return memory[y][x]
+	}
+
+	res := 0
+	if y == 0 {
+		res = max(upDown(prices, fee, x+1, 0), -prices[x]+upDown(prices, fee, x+1, 1))
+	} else {
+		res = max(prices[x]-fee+upDown(prices, fee, x+1, 0), upDown(prices, fee, x+1, 1))
+	}
+	memory[y][x] = res
+
+	return res
 }
 
 func max(x, y int) int {
