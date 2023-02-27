097a113390c0d245837657a50d9a149c5c50f46d
huangyuli <allbuleyu@163.com>
2023-02-27 22:18:58 +0800

    792

--- leetcode/0792-number-of-matching-subsequences/0792.go
+++ leetcode/0792-number-of-matching-subsequences/0792.go
@@ -1,1 +1,18 @@
 package prob0792
+
+func numMatchingSubseq(s string, words []string) (num int) {
+	waiting := map[rune][]string{' ': words}
+	for _, c := range " " + s {
+		advance := waiting[c]
+		delete(waiting, c)
+		for _, word := range advance {
+			if len(word) == 0 {
+				num++
+			} else {
+				c := rune(word[0])
+				waiting[c] = append(waiting[c], word[1:])
+			}
+		}
+	}
+	return
+}
--- leetcode/0792-number-of-matching-subsequences/0792_test.go
+++ leetcode/0792-number-of-matching-subsequences/0792_test.go
@@ -1,1 +1,39 @@
 package prob0792
+
+import (
+	"testing"
+)
+
+func Test_numMatchingSubseq(t *testing.T) {
+	type args struct {
+		s     string
+		words []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantNum int
+	}{
+		{"x", args{
+			s:     "agbcde",
+			words: []string{"a", "bb", "acd", "ace"},
+		}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotNum := numMatchingSubseq(tt.args.s, tt.args.words); gotNum != tt.wantNum {
+				t.Errorf("numMatchingSubseq() = %v, want %v", gotNum, tt.wantNum)
+			}
+		})
+	}
+}
+
+func Test_Map(t *testing.T) {
+	m := map[int]int{}
+
+	for i := 0; i < 10; i++ {
+		m[i] = i
+	}
+
+	t.Log(m[111])
+}
